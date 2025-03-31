d356963100cdf27b7110ac900fd7cf8d34a66242
Matheus Nojoza <141692352+matheusharlen@users.noreply.github.com>
2025-03-31 04:42:56 +0000

    rota categorias

--- api-server/routes/listas.js
+++ api-server/routes/listas.js
@@ -15,4 +15,9 @@ router.post('/:id/itens', auth, listaController.addItem);
 router.put('/:id/itens/:itemId', auth, listaController.updateItem);
 router.delete('/:id/itens/:itemId', auth, listaController.deleteItem);
 
+// Categorias
+router.post('/:id/categorias', auth, listaController.addCategory);
+router.put('/:id/categorias/:catId', auth, listaController.updateCategory);
+router.delete('/:id/categorias/:catId', auth, listaController.deleteCategory);
+
 module.exports = router;
