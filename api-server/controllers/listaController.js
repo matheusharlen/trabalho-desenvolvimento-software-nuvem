cf7b43e8ba898f3e070b57741609184d7eafecee
SILVANIRNASC <140619070+SILVANIRNASC@users.noreply.github.com>
2025-03-31 04:24:46 +0000

    controller atualizar lista

--- api-server/controllers/listaController.js
+++ api-server/controllers/listaController.js
@@ -29,3 +29,22 @@ exports.createLista = async (req, res) => {
     res.status(500).send('Erro no servidor ao criar lista');
   }
 };
+
+
+// Atualizar nome da lista
+exports.updateLista = async (req, res) => {
+  const { nome } = req.body;
+  try {
+    let lista = await Lista.findById(req.params.id);
+    if (!lista) return res.status(404).json({ msg: 'Lista não encontrada' });
+    if (lista.usuarioId.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Não autorizado' });
+    }
+    lista.nome = nome || lista.nome;
+    lista = await lista.save();
+    req.io.to(`user_${req.user.id}`).emit('lista_atualizada', lista);
+    res.json(lista);
+  } catch (err) {
+    res.status(500).send('Erro no servidor ao atualizar lista');
+  }
+};
