72aa0552a50a28863c5ea811a777b6a539062731
SILVANIRNASC <140619070+SILVANIRNASC@users.noreply.github.com>
2025-03-31 04:24:02 +0000

    controller criação lista

--- api-server/controllers/listaController.js
+++ api-server/controllers/listaController.js
@@ -10,3 +10,22 @@ exports.getListas = async (req, res) => {
     res.status(500).send('Erro no servidor ao obter listas (api)');
   }
 };
+
+
+// Criar uma nova lista
+exports.createLista = async (req, res) => {
+  const { nome } = req.body;
+  try {
+    const newLista = new Lista({
+      nome,
+      usuarioId: req.user.id,
+      itens: [],
+      categorias: [],
+    });
+    const lista = await newLista.save();
+    req.io.to(`user_${req.user.id}`).emit('lista_nova', lista);
+    res.json(lista);
+  } catch (err) {
+    res.status(500).send('Erro no servidor ao criar lista');
+  }
+};
